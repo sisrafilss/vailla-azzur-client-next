2aed0a273b7b819567076d2fb0d164d4a7af7681
Israfil Hossen <sisrafilss@gmail.com>
2022-02-14 15:22:06 +0600

    about-section module css added

--- components/about.js
+++ components/about.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import sign from "../images/sign.png";
+import styles from "../styles/About.module.css";
 import about1 from "../upload/about1.jpg";
 import about2 from "../upload/about2.jpg";
 import about3 from "../upload/about3.jpg";
@@ -7,7 +8,7 @@ import about4 from "../upload/about4.jpg";
 
 function About() {
   return (
-    <section id="about-section">
+    <section id={styles.about_section}>
       <div className="container">
         <div className="title-section discover_our_story" data-aos="zoom-in">
           <h1>Discover Our Story</h1>
@@ -17,22 +18,22 @@ function About() {
             <br />{" "}
           </p>
         </div>
-        <div className="about-box">
+        <div className={styles.about_box}>
           <div className="row">
             <div className="col-md-6">
-              <div className="image-holder our_history_images">
-                <div className="img1">
+              <div className={styles.image_holder + " our_history_images"}>
+                <div className={styles.img1}>
                   <Image src={about1} alt="" />
                 </div>
-                <div className="img2">
+                <div className={styles.img2}>
                   <Image className="snd-img" src={about2} alt="" />
                 </div>
               </div>
             </div>
             <div className="col-md-6">
-              <div className="rest-about-box our_history">
-                <h1>Our History</h1>
-                <p>
+              <div className={styles.rest_about_box + " our_history"}>
+                <h1 className={styles.about_box_heading}>Our History</h1>
+                <p className={styles.rest_about_box_p}>
                   VILLA AZZUR EXUDES AN ATMOSPHERE REMINISCENT OF SOUTH OF
                   FRANCE MEETS PARISIAN GLAMOUR LOCATED AT BANANI. PERFECT FOR
                   ANY SPECIAL OCCASION, PRIVATE AND GROUP DINING, OR ROMANTIC
@@ -46,15 +47,15 @@ function About() {
         <div className="about-box left-content">
           <div className="row">
             <div className="col-md-6">
-              <div className="rest-about-box our_restaurant">
-                <h1>Our Restaurant</h1>
-                <p>
+              <div className={styles.rest_about_box2 + " our_restaurant"}>
+                <h1 className={styles.about_box_heading}>Our Restaurant</h1>
+                <p className={styles.rest_about_box_p}>
                   PERFECT FOR ANY SPECIAL OCCASION, PRIVATE AND GROUP DINING, OR
                   ROMANTIC DINNER, VILLA AZUR IS A TOUCH OF CLASS INFUSED WITH
                   DECADENCE
                 </p>
                 <p>
-                  <span>
+                  <span className={styles.rest_about_box_span}>
                     If you have any questions, we are here to support you 24/7.
                     We do our best to make your life happier and easier! See you
                     soon!
@@ -63,8 +64,10 @@ function About() {
                 <div className="about-time">
                   <div className="row">
                     <div className="col-md-5 col-sm-6">
-                      <p>Monday — Sunday</p>
-                      <span>12 pm — 11 pm</span>
+                      <p className={styles.about_time_p}>Monday — Sunday</p>
+                      <span className={styles.about_time_span}>
+                        12 pm — 11 pm
+                      </span>
                     </div>
                     {/*  <div className="col-md-5 col-sm-6">
 											<p>Friday — Sunday</p>
@@ -75,11 +78,15 @@ function About() {
               </div>
             </div>
             <div className="col-md-6">
-              <div className="image-holder our_restaurant_images">
-                <div className="img1">
+              <div
+                className={
+                  styles.our_restaurant_images + " our_restaurant_images"
+                }
+              >
+                <div className={styles.img1}>
                   <Image src={about4} alt="" />
                 </div>
-                <div className="img2">
+                <div className={styles.img2}>
                   <Image className="snd-img" src={about3} alt="" />
                 </div>
               </div>
