a9b267f45ec294ec1dc2dd6bd5be37ad9318d3c6
Israfil Hossen <sisrafilss@gmail.com>
2022-02-15 17:58:42 +0600

    Make Reservation section code added

--- components/makeReservation.js
+++ components/makeReservation.js
@@ -1,8 +1,65 @@
 function MakeReservation() {
   return (
-    <>
-      <h2>Make Reservation</h2>
-    </>
+    <section id="reservation-section">
+      <div className="container">
+        <div className="reservation-box">
+          <div className="row">
+            <div className="col-md-6 make_reservation">
+              <div className="title-section white">
+                <h1>Make Reservation</h1>
+                <p>
+                  Booking a table has never been so easy with free online <br />{" "}
+                  restaurant reservations, booking now!!
+                </p>
+              </div>
+              <form className="reservation-form">
+                <label>Guest Name:</label>
+                <input
+                  type="text"
+                  name="name-guest"
+                  id="name-guest"
+                  placeholder="Name"
+                />
+                <label>Date:</label>
+                <input
+                  type="text"
+                  name="date-guest"
+                  id="date-guest"
+                  placeholder="mm/dd/yyyy"
+                />
+                <div className="row">
+                  <div className="col-md-6">
+                    <label>Time:</label>
+                    <input
+                      type="text"
+                      name="time-guest"
+                      id="time-guest"
+                      placeholder="7:00 pm"
+                    />
+                  </div>
+                  <div className="col-md-6">
+                    <label>Pers:</label>
+                    <input
+                      type="text"
+                      name="pers-guest"
+                      id="pers-guest"
+                      placeholder="2 people"
+                    />
+                  </div>
+                </div>
+                <input
+                  type="submit"
+                  name="submit-reservation"
+                  id="submit-reservation"
+                  value="Find a Table"
+                />
+                <div id="msg-rsv"></div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
